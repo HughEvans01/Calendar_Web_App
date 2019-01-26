94540ef5ca4d6ff77b6163f95bc0457c02f4025b
Hugh Evans <hughevaans01@outlook.com>
2019-01-26 13:10:24 +0000

    "Movable Feast" Update
    Added function which allows special days without fixed date to be added to calendar. Added Easter.

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
-$(document).ready(loadYear);
+$(document).ready(load);
 
 var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "Novemeber", "December"];
+const now = new Date();
+
+function load() {
+  /* Setup all features of Web Calendar App */
+  populateYear()
+  loadYear();
+}
+
+function populateYear() {
+  /* Populate special days array with days without fixed dates*/
+  var year = now.getFullYear();
+  var month = 0;
+  var day = 0;
+  //Easter
+  /*Credit https://gist.github.com/johndyer/0dffbdd98c2046f41180c051f378f343 */
+  var f = Math.floor,
+  // Golden Number - 1
+  G = year % 19,
+  C = f(year / 100),
+  // related to Epact
+  H = (C - f(C / 4) - f((8 * C + 13)/25) + 19 * G + 15) % 30,
+  // number of days from 21 March to the Paschal full moon
+  I = H - f(H/28) * (1 - f(29/(H + 1)) * f((21-G)/11)),
+  // weekday for the Paschal full moon
+  J = (year + f(year / 4) + I + 2 - C + f(C / 4)) % 7,
+  // number of days from 21 March to the Sunday on or before the Paschal full moon
+  L = I - J,
+  month = 3 + f((L + 40)/44),
+  day = L + 28 - 31 * f(month / 4);
+  specialDays[month-1][day-1] = "Easter Sunday";
+}
 
 function loadYear() {
-  const now = new Date();
+  /* Load calendar feature */
   for (i = 0; i < 12; i++) {
     const monthLength = new Date(now.getFullYear(), i - 1, 0).getDate();
     const firstDay = new Date(now.getFullYear(), i, 1);
